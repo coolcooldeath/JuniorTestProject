8a2e2c7a5ee91305a264da2af9013f02e8f20c64
coolcooldeath <dobre4ko@gmail.com>
2022-10-09 16:31:25 +0300

    routing fix

--- app/index.php
+++ app/index.php
@@ -5,17 +5,17 @@ session_start();
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $segments = explode('/', trim($uri, '/'));
-if ($segments[2] === 'login') {
-
+if ($segments[2] === 'login'&&empty($segments[3]))
     include 'login.php';
-} else if ($segments[2] === 'registry') {
-
+else if ($segments[2] === 'registry'&&empty($segments[3]))
     include 'signup.php';
-} else if ($segments[2] === 'hello_user' && AuthClass::isAuth())
+else if (empty($segments[2]))
+    include 'signup.php';
+else if ($segments[2] === 'hello_user' && AuthClass::isAuth() &&empty($segments[3]))
     include 'hello_user.php';
-else if ($segments[2] === 'hello_user')
+else if ($segments[2] === 'hello_user' &&empty($segments[3]))
     include 'login.php';
-else if ($segments[2] === 'php')
+else if ($segments[2] === 'php' &&empty($segments[3]))
     include 'login.php';
 else
     include 'error404.php';
